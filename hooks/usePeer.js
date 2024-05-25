9451a8860be2b8eeae052f2afce95ec7296bc8ec
ayush-baliyan-19 <ayushbaliyan05@gmail.com>
2024-05-25 19:46:18 +0530

    refactor: Lazily import Peer module in usePeer hook

--- hooks/usePeer.js
+++ hooks/usePeer.js
@@ -1,23 +1,24 @@
 const { useEffect, useState, useRef } = require("react");
 import { useSocket } from "@/context/socket";
 import { usePathname } from "next/navigation";
-import Peer from "peerjs";
 const usePeer = () => {
-    const {socket,userName}= useSocket();
-    const pathName=usePathname()
-    const roomId= pathName?.slice(1,pathName.length);
+    const { socket, userName } = useSocket();
+    const pathName = usePathname()
+    const roomId = pathName?.slice(1, pathName.length);
     const [peer, setPeer] = useState(null);
     const [myId, setId] = useState('');
     const isPeerSet = useRef(false);
     useEffect(() => {
-        if(isPeerSet.current || !roomId   || !socket) return;
-        isPeerSet.current = true;
-        const newPeer = new Peer([userName], {secure:true});
-        setPeer(newPeer);
+        import("peerjs").then(({ default: Peer }) => {
+            if (isPeerSet.current || !roomId || !socket) return;
+            isPeerSet.current = true;
+            const newPeer = new Peer([userName], { secure: true });
+            setPeer(newPeer);
 
-        newPeer.on("open", (id) => {
-            setId(userName);
-            socket?.emit('join-room',roomId,userName)
+            newPeer.on("open", (id) => {
+                setId(userName);
+                socket?.emit('join-room', roomId, userName)
+            });
         });
 
     }, [roomId, socket]);
